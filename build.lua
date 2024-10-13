5e396e06860f56de4bfda55d5bbb9ac49a6166bb
Pablo González L <pablgonz@yahoo.com>
2024-10-13 00:53:50 -0300

    try 21

--- build.lua
+++ build.lua
@@ -278,7 +278,7 @@ if options["target"] == "testpkg" then
   -- Compiling sample files
   print("Compiling sample files in ./"..tmpdir.." using [arara]")
   for i, samples in ipairs(samples) do
-    errorlevel = run(tmpdir, "lualatex "..samples..".tex > "..os_null)
+    errorlevel = run(tmpdir, "lualatex-dev "..samples..".tex > "..os_null)
     if errorlevel ~= 0 then
       local f = assert(io.open(tmpdir.."/"..samples..".log", "r"))
       err_log_file = f:read("*all")
