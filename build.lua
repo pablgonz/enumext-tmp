8bbfd28b83d1a2abae3047fb6680fee290cde382
Pablo González L <pablgonz@yahoo.com>
2024-10-22 12:38:56 -0300

    Update tl pkgs

--- build.lua
+++ build.lua
@@ -253,8 +253,8 @@ if options["target"] == "testpkg" then
   -- Compiling test files for "testpkg" target
   local samples = {"enumext-01", "enumext-02", "enumext-03", "enumext-04", "enumext-05"}
   print("Compiling tagged PDF sample files in ./"..tmpdir.." using [arara]")
-  run(tmpdir, "ls -lh")
-  run(tmpdir, "cat enumext-01.tex")
+  --run(tmpdir, "ls -lh")
+  run(tmpdir, "arara enumext-01.tex")
   for i, samples in ipairs(samples) do
     local errorlevel = run(tmpdir, "arara "..samples..".tex > "..os_null)
     if errorlevel ~= 0 then
